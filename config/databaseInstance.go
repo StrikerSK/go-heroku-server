1caf256b9eccf017029aed3a271b6e5144eb5ffb
Teh|Striker <kristianhanus@gmail.com>
2023-12-24 11:23:27 +0100

    Database Singleton adjustmens

--- config/databaseInstance.go
+++ config/databaseInstance.go
@@ -15,31 +15,25 @@ var (
 	databaseConnection *gorm.DB
 )
 
-func InitializeSQLiteDatabase() {
-	databaseLock.Lock()
-	defer databaseLock.Unlock()
+func InitializeDefaultSQLiteDatabase() {
+	InitializeSQLiteDatabase("file::memory:?cache=shared")
+}
 
-	// Check if the database connection is already created
-	if databaseConnection != nil {
-		log.Println("Application Database instance already created.")
-		return
-	}
-
-	log.Println("Creating Database instance")
-	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
-	if err != nil {
-		log.Printf("Database initialization: %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	databaseConnection = db
+func InitializeSQLiteDatabase(dsn string) {
+	dialector := sqlite.Open(dsn)
+	InitializeDatabase(dialector)
 }
 
 func InitializeDefaultPostgresDatabase() {
 	InitializePostgresDatabase("localhost", "5432", "postgres", "postgres", "postgres", "disable")
 }
 
-func InitializePostgresDatabase(host, port, dbname, user, password, sslmode string) {
+func InitializePostgresDatabase(host, port, dbname, user, password, sslMode string) {
+	args := fmt.Sprintf("host=%s port=%s dbname=%s user='%s' password=%s sslmode=%s", host, port, dbname, user, password, sslMode)
+	InitializeDatabase(postgres.Open(args))
+}
+
+func InitializeDatabase(dialector gorm.Dialector) {
 	databaseLock.Lock()
 	defer databaseLock.Unlock()
 
@@ -50,8 +44,7 @@ func InitializePostgresDatabase(host, port, dbname, user, password, sslmode stri
 	}
 
 	log.Println("Creating Database instance")
-	args := fmt.Sprintf("host=%s port=%s dbname=%s user='%s' password=%s sslmode=%s", host, port, dbname, user, password, sslmode)
-	db, err := gorm.Open(postgres.Open(args), &gorm.Config{})
+	db, err := gorm.Open(dialector, &gorm.Config{})
 	if err != nil {
 		log.Printf("Database initialization: %s\n", err.Error())
 		os.Exit(1)
